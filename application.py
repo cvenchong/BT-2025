71bb05c56b72edb0bd6fe122f778ce1ad2d57024
cvenchong <cvenchong@gmail.com>
2025-06-10 20:56:21 +0800

    Add files via upload

--- application.py
+++ application.py
@@ -241,7 +241,7 @@ def createCustomerAccount(cid):
 
 # Run the backend app
 if __name__ == "__main__":
-#    app.run(debug=True)
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(debug=True)
+#    app.run(host="0.0.0.0", port=8000, debug=True)
 #    app.run(debug=True, host="0.0.0.0", port=50000)
 
