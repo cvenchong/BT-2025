e93287d88933f037af78333dc92d27c7f15bd70a
cvenchong <cvenchong@gmail.com>
2025-10-22 10:02:14 +0800

    Update index.js

--- static/public/paypal/api-only/appswitch/index.js
+++ static/public/paypal/api-only/appswitch/index.js
@@ -177,7 +177,6 @@ function setReturnFlowHandling() {
     document.addEventListener('hashchange', (e) => {
       console.log('this is auto flow return handling... ')
       const params = parseHashParams(window.location.hash);
-      console.log('Parsed hash parameters:', params);
       if (params.approved) {
         console.log('Buyer is returning from app switch with an approved order');
         // Buyer is returning from app switch with an approved order
@@ -193,15 +192,21 @@ function setReturnFlowHandling() {
     //init manual flow handling
     document.addEventListener('visibilitychange', (e) => {
       // call your server API to make a request to PayPal to get the order status and buyer cancellation status (if applicable)
+      console.log('Visibility changed. Checking order status...');
 
       //If #change event was triggered then cancel this event (to avoid multiple payment/order calls from both the listeners)
       const hashParams = parseHashParams(window.location.hash);
       if (hashParams.approved || hashParams.cancelled) {
+        console.log('Hash parameters indicate approval or cancellation.');
+        //console log all the hash parameters 
+        console.log('Hash parameters:', hashParams);
         //Wil be handled by hashChange. Exit
         return;
       }
 
       const orderResponse = getOrderResponse(orderId);
+      console.log('Order response:', orderResponse);
+
       const orderStatus = orderResponse.status;
       const cancelledActivity = orderResponse?.payment_source?.paypal?.experience_status;
 
@@ -350,9 +355,18 @@ function initConfig() {
     // payload.orderID = orderID;
     // payload.customID = customID;
 
-    //detect if app switch supported. if not, opt out
+    console.log('app switch experience enabled? ', config.enableAppSwitch);
+    //detect if app switch enabled. if not, opt out
+    if (!config.enableAppSwitch) {
+      console.log("App Switch is not enabled. Opting out. Removing app_switch_context from payload.");
+      //remove app_switch_context from payload
+      delete payload.app_switch_context;
+    }else{
+      setReturnFlowHandling();
+    }
 
-    console.log("Final payload:", payload);
+
+    console.log("Final Create Order Payload:", payload);
     console.log("Sending order creation request...");
     //send order creation request to server
     fetch('/create_pp_order_v2_raw_payload', {
@@ -375,8 +389,6 @@ function initConfig() {
         // and that PayPal has found the payer eligible. The merchant should attempt to App Switch. 
         //app_switch_eligibility = true does not guarantee that the payer will switch to the PayPal app. 
         // It only indicates that PayPal requests the merchant to attempt App Switch for the payer.
-        console.log('app switch experience enabled? ', config.enableAppSwitch);
-        console.log('this is a experience for return_flow: ', config.returnFlow);
         let app_switch_eligibility = order_response?.payment_source?.paypal?.app_switch_eligibility ?? false;
         console.log("App Switch Eligibility: ", app_switch_eligibility);
 
@@ -452,9 +464,13 @@ function parseHashParams(hash) {
   // Remove leading '#' if present
   hash = hash.startsWith('#') ? hash.substring(1) : hash;
   const params = {};
+  // console log all the key value pairs as they are parsed
   hash.split('&').forEach(function (kv) {
     const [key, value] = kv.split('=');
-    if (key) params[key] = value || true;
+    if (key) {
+      params[key] = value || true;
+      console.log(`Parsed hash param: ${key} = ${value || true}`);
+    }
   });
   return params;
 }
@@ -488,220 +504,5 @@ function main() {
   //auto flow
   onLoadHash();
 }
-
-// async function getClientToken() {
-//   try {
-//     const clientToken = await fetch('/getPPClientToken', {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//       }
-//     });
-
-//     const clientTokenJson = await clientToken.json();
-//     console.log("Client Token: ", clientTokenJson.token);
-
-//     return clientTokenJson.token;
-//   } catch (err) {
-//     throw new Error(err);
-//     return null;
-//   }
-// }
-
-// async function createOrder() {
-//   try {
-//     const order = await fetch('/create_pp_order_v2', {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         vaultOnSuccess: "CAPTURE",
-//       }),
-//     });
-
-//     const orderData = await order.json();
-//     console.log("Order created:", orderData);
-//     return orderData;
-//   } catch (error) {
-//     console.error("Error creating order:", error);
-//   }
-// }
-
-// function main() {
-//   // eslint-disable-next-line no-undef
-//   if ((paypal ?? null) === null) {
-//     console.error("No paypal object with associated loaded PayPal script");
-//     document.getElementById("feedback").innerHTML = "Error loading scripts";
-//     document.getElementById("error").innerHTML =
-//       "No paypal object with associated loaded PayPal script";
-//     return;
-//   }
-//   // eslint-disable-next-line no-undef
-//   if ((paypal.Buttons ?? null) === null) {
-//     console.error("No Buttons method with associated paypal object");
-//     document.getElementById("feedback").innerHTML = "Error loading scripts";
-//     document.getElementById("error").innerHTML =
-//       "No Buttons method with associated paypal object";
-//     return;
-//   }
-
-//   // eslint-disable-next-line no-undef
-//   const buttons = paypal.Buttons({
-//     appSwitchWhenAvailable: true,
-//     async createOrder() {
-//       console.log("Received order creation request");
-
-//       console.log("Attempting to create order");
-//       return fetch(`${BASE_URL}/paypal/rest/order/new`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           intent: "CAPTURE",
-//           purchaseUnits: [
-//             {
-//               reference_id: "asdf",
-//               description: "qwerty",
-//               amount: {
-//                 currency_code: "USD",
-//                 value: "78.50",
-//               },
-//               // crypto.randomUUID() which is usually used to generate a UUID isn't available on Chrome on Android
-//               // invoice_id: crypto.randomUUID(),
-//               invoice_id: "10000000-1000-4000-8000-100000000000".replace(
-//                 /[018]/g,
-//                 (c) =>
-//                   (
-//                     +c ^
-//                     (crypto.getRandomValues(new Uint8Array(1))[0] &
-//                       (15 >> (+c / 4)))
-//                   ).toString(16)
-//               ),
-//             },
-//           ],
-//           paymentSource: {
-//             paypal: {
-//               experience_context: {
-//                 user_action: "PAY_NOW",
-//                 // use 10.0.2.2 URL when using localhost/10.0.2.2, use playgroundappswitch.infinityfreeapp.com when hosting this on the web domain
-//                 return_url:
-//                   "http://10.0.2.2:5500/public/paypal/js-sdk/rest/app-switch/index.html",
-//                 cancel_url:
-//                   "http://10.0.2.2:5500/public/paypal/js-sdk/rest/app-switch/index.html",
-//                 // return_url:
-//                 //   "https://playgroundappswitch.infinityfreeapp.com/paypal/index.html",
-//                 // cancel_url:
-//                 //   "https://playgroundappswitch.infinityfreeapp.com/paypal/index.html",
-//                 app_switch_preference: {
-//                   launch_paypal_app: true,
-//                 },
-//               },
-//             },
-//           },
-//           merchantCountry: "US",
-//         }),
-//       })
-//         .then(async (response) => {
-//           if (response.status === 201) {
-//             return response.json();
-//           } else {
-//             throw new Error((await response.json())?.message);
-//           }
-//         })
-//         .then((data) => {
-//           if ((data?.id ?? null) === null) {
-//             throw new Error("No order ID with associated created order");
-//           }
-
-//           return data.id;
-//         })
-//         .then((orderId) => {
-//           console.log(`Order with ID ${orderId} successfully created`);
-//           document.getElementById(
-//             "feedback"
-//           ).innerHTML = `Order with ID ${orderId} successfully created`;
-//           document.getElementById("error").innerHTML = "";
-//           return orderId;
-//         })
-//         .catch(() => {
-//           document.getElementById("feedback").innerHTML =
-//             "Error creating order";
-//           return undefined;
-//         });
-//     },
-//     onApprove(data) {
-//       if ((data ?? null) === null) {
-//         document.getElementById("feedback").innerHTML = "Error approving order";
-//         throw new Error("No data object with associated approval data");
-//       }
-//       if ((data.orderID ?? null) === null) {
-//         document.getElementById("feedback").innerHTML = "Error approving order";
-//         throw new Error("No orderID attribute with associated data object");
-//       }
-
-//       console.log("Received order approval event");
-
-//       console.log("Attempting to capture order");
-//       fetch(`${BASE_URL}/paypal/rest/order/capture`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           orderId: data.orderID,
-//           merchantCountry: "US",
-//         }),
-//       })
-//         .then(async (response) => {
-//           if (response.status === 201) {
-//             console.log(`Order with ID ${data.orderID} successfully captured`);
-//             document.getElementById(
-//               "feedback"
-//             ).innerHTML = `Order with ID ${data.orderID} successfully captured`;
-//             document.getElementById("error").innerHTML = "";
-//           } else {
-//             throw new Error((await response.json())?.message);
-//           }
-//         })
-//         .catch(() => {
-//           document.getElementById("feedback").innerHTML =
-//             "Error approving order";
-//         });
-//     },
-//     onCancel(data) {
-//       if ((data ?? null) === null) {
-//         document.getElementById("feedback").innerHTML =
-//           "Error in cancel order callback";
-//         throw new Error("No data object with associated cancel data");
-//       }
-//       if ((data.orderID ?? null) === null) {
-//         document.getElementById("feedback").innerHTML =
-//           "Error in cancel order callback";
-//         throw new Error("No orderID attribute with associated data object");
-//       }
-
-//       document.getElementById(
-//         "feedback"
-//       ).innerHTML = `Order with ID ${data.orderID} was cancelled`;
-//       document.getElementById("error").innerHTML = "";
-//       console.log(`Order with ID ${data.orderID} was cancelled`);
-//     },
-//     onError(error) {
-//       console.error(error);
-//       document.getElementById("error").innerHTML = error.message;
-//     },
-//   });
-
-//   if (buttons.hasReturned()) {
-//     console.log("Received buttons returned callback");
-//     buttons.resume();
-//   } else {
-//     buttons.render("#container");
-//   }
-// }
-
-
 
 main();
